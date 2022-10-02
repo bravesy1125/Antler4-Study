63e2404c25f8d8941b9c02eec30ef12a37974097
shi48 <Yong.Shi.ext@imec.be>
2022-10-02 10:31:10 +0200

    add new folder mat_py to save python files

--- test.bat
+++ test.bat
@@ -1,6 +1,6 @@
 taskkill /im java.exe /f
 antlr4 MAT.g4
-antlr4 -Dlanguage=Python3 MAT.g4
+antlr4 -Dlanguage=Python3 MAT.g4 -long-messages -o mat_py
 javac MAT*.java
 python main.py mattest.mat
 start /b grun MAT complete -gui mattest.mat
